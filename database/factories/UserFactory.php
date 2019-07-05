1766061e86b2b9b5019682a234b2d76cb995923d
Monday Victor A <vkingmaker1@gmail.com>
2019-07-05 13:50:23 +0100

    feature(thread): add view and browser thread
    This commit adds the features to enable the user browse and/or view a particular thread
    
    Delivers [#thread]

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\User;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(App\Thread::class, function (Faker $faker){
+    return [
+        'user_id'=> factory(User::class)->create()->id,
+        'title' => $faker->sentence,
+        'body'=> $faker->paragraph
+    ];
+});
+
+$factory->define(App\Reply::class, function(Faker $faker) {
+    return [
+        'thread_id'=> factory(App\Thread::class)->create()->id,
+        'user_id' => factory(App\User::class)->create()->id,
+        'body' => $faker->paragraph
+    ];
+});
