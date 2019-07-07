e567a219fb3e2f44e56c4730df7bb717180b4666
Monday Victor A <vkingmaker1@gmail.com>
2019-07-07 03:51:05 +0100

    feature(validateReplyAndThreads): add validation for thread and reply
    This commit adds validation for threads and replies to ensure the required fields are been sent to be saved in the database
    
    Deliver [#validateReplyAndThreads]

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -11,7 +11,6 @@ class CreateThreadsTest extends TestCase
     /** @test */
     public function guest_may_not_create_threads()
     {
-        // $this->withoutExceptionHandling();
 
         $this->get('/threads/create')
 
@@ -28,13 +27,53 @@ class CreateThreadsTest extends TestCase
    {
     $this->signIn();
 
-    $thread = create('App\Thread');
+    $thread = make('App\Thread');
 
-    $this->post('/threads', $thread->toArray());
+    $response = $this->post('/threads', $thread->toArray());
 
-    $this->get($thread->path())->assertSee($thread->title)
+    $this->get($response->headers->get('Location'))->assertSee($thread->title)
 
         ->assertSee($thread->body);
+
+   }
+
+   /** @test */
+   public function a_thread_requires_a_title()
+   {
+       $this->publishThread(['title' => null ])
+
+            ->assertSessionHasErrors('title');
+   }
+
+   /** @test */
+   public function a_thread_requires_a_body()
+   {
+       $this->publishThread(['body' => null ])
+
+            ->assertSessionHasErrors('body');
+   }
+
+   /** @test */
+   public function a_thread_requires_a_valid_channel()
+   {
+       factory('App\Channel',2)->create();
+
+       $this->publishThread(['channel_id' => null ])
+
+            ->assertSessionHasErrors('channel_id');
+
+       $this->publishThread(['channel_id' => 999 ])
+
+            ->assertSessionHasErrors('channel_id');
+   }
+
+   public function publishThread($overrides = [])
+   {
+       $this->signIn();
+
+       $thread = make('App\Thread', $overrides);
+
+       return $this->post('/threads', $thread->toArray());
 
    }
 }
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -35,4 +35,18 @@ class ParticipateInForumTest extends TestCase
        ->assertSee($reply->body);
 
    }
+
+   /** @test */
+   public function a_reply_requires_a_body()
+   {
+    $this->signIn();
+
+    $thread = create('App\Thread');
+
+    $reply = make('App\Reply', ['body' => null ]);
+
+    $this->post($thread->path().'/replies', $reply->toArray())
+
+        ->assertSessionHasErrors('body');
+   }
 }
