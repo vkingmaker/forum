ed5410b73f6ae1f4df3d4b208ab2738827004431
Monday Victor A <vkingmaker1@gmail.com>
2019-07-11 15:12:57 +0100

    chore(notifyAllSubscribers): notify every subscribed user
    This commit adds a test case to make sure all the subscribed users are notified when a new reply is added
    
    Delivers [#notifyAllSubscribers]

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\ThreadWasUpdated;
 
 class ThreadTest extends TestCase
 {
@@ -50,6 +52,19 @@ class ThreadTest extends TestCase
         ]);
 
         $this->assertCount(1,$this->thread->replies);
+    }
+
+    /** @test */
+    public function a_thread_notifies_all_registered_subscribers_when_a_reply_is_added()
+    {
+        Notification::fake();
+
+        $this->signIn()->thread->subscribe()->addReply([
+            'body' => 'Foobar',
+            'user_id' => 999
+        ]);
+
+        Notification::assertSentTo(auth()->user(), ThreadWasUpdated ::class);
     }
 
     /** @test */
